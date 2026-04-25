faf29e0a54705ecf0d804c4e78bdd8c5a7acce98
mizanurcodes-ux <mizanur.codes@gmail.com>
2026-04-25 20:01:37 +0600

    fix: move mongodb uri to env

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb+srv://Tarek_auth:bMTbFSaKyGiqnQJL@cluster0.uaz5xk0.mongodb.net/?appName=Cluster0");
+const client = new MongoClient(process.env.MONGO_URI);
 const db = client.db();
 
 export const auth = betterAuth({
