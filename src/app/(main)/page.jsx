aac0d915f534c15b6498be25b53e8522e757e503
mizanurcodes-ux <mizanur.codes@gmail.com>
2026-04-30 11:39:46 +0600

    fix login with google

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -5,10 +5,10 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      {/* হিরো সেকশন */}
+     
       <Hero />
 
-      {/* এখানে পরে কোর্সের লিস্ট বা অন্য কন্টেন্ট যোগ করতে পারবেন */}
+     
       <section className="container mx-auto py-20 px-4 text-center">
         <h2 className="text-3xl font-bold text-gray-800">
           Start Your Learning Journey
